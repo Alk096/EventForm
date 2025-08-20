4c7fc75f534175082ab037bdb18bcd7ed1c67b3d
Alk096 <mahamsdoualker@gmail.com>
2025-08-20 23:11:31 +0100

    Generate QR code

--- eventForm.js
+++ eventForm.js
@@ -1,23 +1,35 @@
 /*=========== Recuperer Les Info du formulaire ================= */
-function getInfo(){
-    const formInfo = {}
-    formInfo.prenom = document.getElementById('prenom').value
-    formInfo.nom = document.getElementById('nom').value
-    formInfo.numDeTel = document.getElementById('numeroDeTel').value
-    formInfo.serieBac = document.getElementById('serieBac').value
+function getInfo() {
+  const formInfo = {}
+  formInfo.prenom = document.getElementById('prenom').value
+  formInfo.nom = document.getElementById('nom').value
+  formInfo.numDeTel = document.getElementById('numeroDeTel').value
+  formInfo.serieBac = document.getElementById('serieBac').value
 
-    const fQ = document.querySelectorAll('.responses')
-    fQ.forEach(( question, index)=>{
-        const input = question.querySelector('input[type ="radio"]:checked')
-        formInfo[input.name] = input ? input.value : null
-    })
-    return formInfo
+  const fQ = document.querySelectorAll('.responses')
+  fQ.forEach((question, index) => {
+    const input = question.querySelector('input[type ="radio"]:checked')
+    formInfo[input.name] = input ? input.value : null
+  })
+  return formInfo
 }
+
+/*========= Generation du QR Code =================*/
+function generateQRCode(data) {
+  document.getElementById("qrcode").innerHTML = "";
+  new QRCode(document.getElementById("qrcode"), {
+    text: JSON.stringify(data),
+    width: 128,
+    height: 128
+  });
+}
+
 
 /*============ Submit ======*/
 const btn = document.getElementById('btn')
-btn.addEventListener('click', (e) =>{
-    e.preventDefault()
-    const Info = getInfo()
-    console.log(Info)
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const info = getInfo()
+  console.log(info)
+  generateQRCode(info)
 })
